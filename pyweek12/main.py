742a800d784a7f24cee1a1cdd522e2259a183f6d
Jonathan Hartley <tartley@tartley.com>
2011-04-04 23:47:47 +0100

    make weeds, flowers and bushes instad of cars

--- pyweek12/main.py
+++ pyweek12/main.py
@@ -7,6 +7,7 @@ import pyglet
 from rabbyt.sprites import Sprite
 
 from .camera import Camera
+from .color import Color
 from .eventloop import Eventloop
 from .options import Options
 from .path import DATA
@@ -17,31 +18,30 @@ from .world import World
 def main():
     options = Options(sys.argv)
     world = World()
+    world.background_color = Color(0.1, 0.3, 0)
 
-    def rotate(item, *_):
-        item.rot += item.angular_velocity
+    def rotate(item, _, dt):
+        item.rot += item.angular_velocity * dt
 
     for _ in xrange(100):
-        item = Sprite(
-            join(DATA, 'images', 'car.png'),
-            x=uniform(-100, 100),
-            y=uniform(-100, 100),
-            scale=uniform(0.1, 0.5),
-            rot=uniform(0, 360),
-        )
-        item.update = rotate
-        item.angular_velocity = uniform(-0.1, +0.1)
-        world.add(item)
+        for image in ['bush.png', 'flower.png', 'weed.png']:
+            item = Sprite(
+                join(DATA, 'images', image),
+                x=uniform(-2000, 2000),
+                y=uniform(-2000, 2000),
+                scale=uniform(0.5, 1),
+                rot=uniform(0, 360),
+                green=uniform(0.6, 1),
+            )
+            world.add(item)
 
     item = Sprite(
         join(DATA, 'images', 'tank.png'),
-        x=uniform(-100, 100),
-        y=uniform(-100, 100),
-        scale=uniform(0.1, 0.5),
-        rot=uniform(0, 360),
+        x=0, y=0,
+        scale=1,
     )
     item.update = rotate
-    item.angular_velocity = uniform(-0.1, +0.1)
+    item.angular_velocity = 20
     world.add(item)
 
     window = pyglet.window.Window(
@@ -50,7 +50,7 @@ def main():
         visible=False,
         resizable=True,
     )
-    camera = Camera((0, 0), 100)
+    camera = Camera((0, 0), 800)
     render = Render(world, camera, options)
     eventloop = Eventloop(window, world, render, options)
     eventloop.run(world.update)
