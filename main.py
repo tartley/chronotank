f0cbd78b269508889eb9f08b4b6e157b50a237c1
Jonathan Hartley <tartley@tartley.com>
2011-04-04 22:28:05 +0100

    rationalise main.py / run_game.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import pyweek12.main
-def main():
-    pyweek12.main.main()
-    
-if __name__ == "__main__":
-    pyweek12.main.main()
-
--- run_game.py
+++ run_game.py
@@ -1,5 +1,3 @@
 import pyweek12.main
-if __name__ == "__main__":
-    pyweek12.main.main()
+pyweek12.main.main()
 
-# vim: set filetype=python sts=4 sw=4 noet si :
