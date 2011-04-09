f07f53458ec9e6b13eaec42b56537c7c7e2b00b4
Jonathan Hartley <tartley@tartley.com>
2011-04-09 16:44:50 +0100

    break Applictaion out into its own module

--- pyweek12/main.py
+++ pyweek12/main.py
@@ -1,22 +1,13 @@
 
-import sys
-
 import pyglet
-from pyglet.event import EVENT_HANDLED
 
-from .camera import Camera
-from .cameraman import CameraMan
-from .collide import Collide
+from .application import Application
 from .color import Color
 from .items.greenery import Tree, Weed, Flower, Fronds
 from .items.hudmessage import LivesMessage, TimeMessage
 from .items.portals import EntryPortal
 from .items.tank import Tank
 from .items.wall import Wall
-from .keyboard import Keyboard
-from .options import Options
-from .render import Render
-from .world import World
 
 
 def populate(world):
@@ -28,60 +19,13 @@ def populate(world):
         world.add( Wall() )
     world.add( EntryPortal(0, 0) )
 
-    
-class Application(object):
-
-    def __init__(self):
-        self.options = Options(sys.argv)
-        self.world = World()
-        self.collide = Collide(self.world)
-        self.world.background_color = Color(0.1, 0.3, 0)
-        populate(self.world)
-        self.window = pyglet.window.Window(
-            fullscreen=self.options.fullscreen,
-            vsync=self.options.vsync,
-            visible=False,
-            resizable=True,
-        )
-        self.camera = Camera((0, 0), scale=16)
-        self.window.on_resize = self.camera.on_resize
-        self.cameraman = CameraMan( self.camera )
-        self.render = Render(self.world, self.camera, self.options)
-        self.keyboard = Keyboard(self.window, self.world, self.options)
-
-    def on_update(self, raw_dt):
-        dt = min(raw_dt, 1 / 30.0)
-        self.world.update_all(dt)
-        self.window.invalid = True
-        self.cameraman.update(dt)
-
-    def on_draw(self):
-        self.render.draw()
-        self.window.invalid = False
-        return EVENT_HANDLED
-
-    def run(self):
-        '''
-        Schedules calls to update, makes window visible and starts the
-        event loop by calling pyglet.app.run()
-        '''
-        pyglet.clock.schedule(self.on_update)
-        self.window.on_draw = self.on_draw
-        self.window.set_visible()
-        self.window.invalid = False
-        try:
-            pyglet.app.run()
-        except:
-            raise
-        finally:
-            self.window.close()
-
-
-
+   
 class Game(Application):
 
     def __init__(self):
         Application.__init__(self)
+        self.world.background_color = Color(0.1, 0.3, 0)
+        populate(self.world)
         self.lives = 9
         pyglet.clock.schedule_once( self.start, 2)
 
